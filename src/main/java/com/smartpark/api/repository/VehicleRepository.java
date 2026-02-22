5c5f5fc1e97b4e208eb466f7dfa54389f8861e23
lynsan11 <lynettesantos19@gmail.com>
2026-02-22 17:28:30 +0800

    Committed

---
+++ src/main/java/com/smartpark/api/repository/VehicleRepository.java
@@ -0,0 +1,8 @@
+package com.smartpark.api.repository;
+
+import com.smartpark.api.models.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface VehicleRepository extends JpaRepository<Vehicle, String>
+{
+}
